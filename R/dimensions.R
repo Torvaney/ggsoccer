ada67dd1fedd0a9eaa8cfb845ec245793dc8c7a1
Torvaney <torvaney@protonmail.com>
2019-04-07 22:07:38 +0100

    Add dimensions

---
+++ R/dimensions.R
@@ -0,0 +1,31 @@
+#' Pitch dimensions
+#'
+#' @export
+pitch_opta <- list(
+  length = 100,
+  width = 100,
+  penalty_box_length = 17,
+  penalty_box_width = 57.8,
+  six_yard_box_length = 5.8,
+  six_yard_box_width = 26.4,
+  penalty_spot_distance = 11.5,
+  goal_width = 11.6,
+  origin_x = 0,
+  origin_y = 0
+)
+
+#' @rdname pitch_opta
+#' @export
+# Source: https://github.com/statsbomb/open-data/blob/master/doc/StatsBomb%20Event%20Data%20Specification%20v1.0.2.pdf
+pitch_statsbomb <- list(
+  length = 120,
+  width = 80,
+  penalty_box_length = 18,
+  penalty_box_width = 44,
+  six_yard_box_length = 6,
+  six_yard_box_width = 20,
+  penalty_spot_distance = 12,
+  goal_width = 8,
+  origin_x = 0,
+  origin_y = 0
+)
